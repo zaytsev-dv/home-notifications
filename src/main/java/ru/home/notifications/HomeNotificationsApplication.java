05f9e1d117b2cf68c410910352708f22e79dea19
zaytsev-dv <zaytsev.dmitry9228@gmail.com>
2020-10-28 19:18:12 +0300

    first commit

---
+++ src/main/java/ru/home/notifications/HomeNotificationsApplication.java
@@ -0,0 +1,15 @@
+package ru.home.notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeNotificationsApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(HomeNotificationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/home/notifications/HomeNotificationsApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.home.notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeNotificationsApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
