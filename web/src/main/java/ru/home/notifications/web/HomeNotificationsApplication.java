8e8812b0392f2c9910ceaccca1c631419ad24f2d
zaytsev-dv <zaytsev.dmitry9228@gmail.com>
2020-10-28 19:30:16 +0300

    add module and dockerfile

--- web/src/main/java/ru/home/notifications/web/HomeNotificationsApplication.java
+++ web/src/main/java/ru/home/notifications/web/HomeNotificationsApplication.java
@@ -1,4 +1,4 @@
-package ru.home.notifications;
+package ru.home.notifications.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
