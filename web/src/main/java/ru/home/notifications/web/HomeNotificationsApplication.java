f57cbac0422828dd38e4fdcdb645fef7b303f5e5
zaytsev-dv <zaytsev.dmitry9228@gmail.com>
2020-11-09 21:14:55 +0300

    hot-fix

--- web/src/main/java/ru/home/notifications/web/HomeNotificationsApplication.java
+++ web/src/main/java/ru/home/notifications/web/HomeNotificationsApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"ru.home.notifications.*"})
-@EntityScan(basePackages = {"ru.home.notifications.*"})
 public class HomeNotificationsApplication
 {
 
